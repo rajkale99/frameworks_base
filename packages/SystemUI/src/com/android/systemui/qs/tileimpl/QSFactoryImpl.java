baf5c3b304d579208fb70c55780198de9a786e26
fusionjack <dogfight60-fusionjack@yahoo.de>
2017-11-15 21:21:31 -0500

    Add Reboot/Recovery QS Tile
    Vectorized Reboot/Recovery QS tile drawables Alex Cruz <du.alexcruz@gmail.com>
    - Resized but reused the power menu reboot icon to keep things uniformed
    - Created a recovery vector that fits the same style too
    
    mydongistiny: Updated priv-app whitelist to fix boot
    
    Change-Id: I0762ffb2b3ddf8c833662a237bde20581e8cc614

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -44,6 +44,7 @@ import com.android.systemui.qs.tiles.HWKeysTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
+import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
@@ -100,7 +101,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<DataSwitchTile> mDataSwitchTileProvider;
     private final Provider<SleepScreenTile> mSleepScreenTileProvider;
     private final Provider<WeatherTile> mWeatherTileProvider;
-
+    private final Provider<RebootTile> mRebootTileProvider;
     private final Lazy<QSHost> mQsHostLazy;
 
     @Inject
@@ -135,7 +136,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<SoundTile> soundTileProvider,
             Provider<DataSwitchTile> dataSwitchTileProvider,
             Provider<SleepScreenTile> sleepScreenTileProvider,
-            Provider<WeatherTile> weatherTileProvider) {
+            Provider<WeatherTile> weatherTileProvider,
+            Provider<RebootTile> rebootTileProvider) {
         mQsHostLazy = qsHostLazy;
         mWifiTileProvider = wifiTileProvider;
         mBluetoothTileProvider = bluetoothTileProvider;
@@ -168,6 +170,7 @@ public class QSFactoryImpl implements QSFactory {
         mDataSwitchTileProvider = dataSwitchTileProvider;
         mSleepScreenTileProvider = sleepScreenTileProvider;
         mWeatherTileProvider = weatherTileProvider;
+        mRebootTileProvider = rebootTileProvider;
     }
 
     public QSTile createTile(String tileSpec) {
@@ -241,6 +244,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mSleepScreenTileProvider.get();
             case "weather":
                 return mWeatherTileProvider.get();
+            case "reboot":
+                return mRebootTileProvider.get();
         }
 
         // Custom tiles
