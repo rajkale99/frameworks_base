509e8646245c94392f855f771b271c856c87b41d
00day0 <therandomuser11@gmail.com>
2020-01-10 14:49:20 +0100

    SystemUI: Move battery icon to QS header
    Change-Id: I74d6197c3f544f18fb86757fecc2bff7eb295ae9
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Alexandru Scurtu <sasha.scurtu@gmail.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -589,6 +589,7 @@ public class BatteryMeterView extends LinearLayout implements
         }
     }
 
+
     private void updateColors(int foregroundColor, int backgroundColor, int singleToneColor) {
         mDrawable.setColors(foregroundColor, backgroundColor, singleToneColor);
         mXDrawable.setColors(foregroundColor, backgroundColor);
