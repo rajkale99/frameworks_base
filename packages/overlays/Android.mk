3c4756662f3028254db90b272c4df2098153f5ca
Lup Gabriel <gwolf2u.website@gmail.com>
2020-01-25 17:22:11 +0200

    Add Cocon and Quando fonts [1/2]

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -53,6 +53,8 @@ LOCAL_REQUIRED_MODULES := \
 	FontSamsungOneSourceOverlay \
 	FontNokiaPureSourceOverlay \
 	FontFifa2018SourceOverlay \
+	FontCoconSourceOverlay \
+	FontQuandoSourceOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularPixelLauncherOverlay \
 	IconPackCircularPixelThemePickerOverlay \
