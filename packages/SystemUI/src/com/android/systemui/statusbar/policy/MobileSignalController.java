719fc1e3ab90706ecd28b6823f7fd2545c7b9144
Alex Yustasov <alex9yust@gmail.com>
2020-05-15 17:08:56 +0530

    Always notify listeners when switching mobile data in QS

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -742,7 +742,7 @@ public class MobileSignalController extends SignalController<
 
     void onMobileDataChanged() {
         checkDefaultData();
-        notifyListenersIfNecessary();
+        notifyListeners();
     }
 
     private MobileIconGroup getNr5GIconGroup() {
