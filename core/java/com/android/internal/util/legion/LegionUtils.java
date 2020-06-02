872de32f1d3ea466ee6fcbcdaa01211b1e612e3a
SpiritCroc <dev@spiritcroc.de>
2020-06-02 12:41:00 +0200

    Utils: Import restart systemui api
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I03715be8271327db2d960506517c3298cb4db638

--- core/java/com/android/internal/util/legion/LegionUtils.java
+++ core/java/com/android/internal/util/legion/LegionUtils.java
@@ -18,11 +18,16 @@ package com.android.internal.util.legion;
 
 import android.content.Context;
 import android.content.Intent;
+import android.app.ActivityManager;
+import android.app.AlertDialog;
 import android.content.pm.PackageManager;
+import android.app.IActivityManager;
 import android.hardware.input.InputManager;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.PowerManager;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
@@ -41,6 +46,7 @@ import android.view.KeyEvent;
 import android.content.om.IOverlayManager;
 import android.content.om.OverlayInfo;
 
+import com.android.internal.R;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import java.util.List;
@@ -105,6 +111,50 @@ public class LegionUtils {
 
         return true;
     }
+
+	public static void restartSystemUi(Context context) {
+        new RestartSystemUiTask(context).execute();
+    }
+
+    public static void showSystemUiRestartDialog(Context context) {
+        new AlertDialog.Builder(context)
+                .setTitle(R.string.systemui_restart_title)
+                .setMessage(R.string.systemui_restart_message)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        restartSystemUi(context);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
+    }
+
+    private static class RestartSystemUiTask extends AsyncTask<Void, Void, Void> {
+        private Context mContext;
+
+        public RestartSystemUiTask(Context context) {
+            super();
+            mContext = context;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                ActivityManager am =
+                        (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+                IActivityManager ams = ActivityManager.getService();
+                for (ActivityManager.RunningAppProcessInfo app: am.getRunningAppProcesses()) {
+                    if ("com.android.systemui".equals(app.processName)) {
+                        ams.killApplicationProcess(app.processName, app.uid);
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+   }
 
     public static boolean isAvailableApp(String packageName, Context context) {
        Context mContext = context;
