916cdd1376ef387cf5d6302a1c0d974c069113f3
rajkale99 <kaleraj.rk@gmail.com>
2020-08-09 20:00:26 +0000

    Revert "Revert "Revert "SystemUI: Add config to show battery in qs header"""
    This reverts commit 5ed273ded0dd220d22a53b436ad6e1760b2998c7.

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -185,7 +185,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
     private boolean mHideDragHandle;
 
     private PrivacyItemController mPrivacyItemController;
-    private BatteryMeterView mBatteryMeterView;
 
     private TextView mSystemInfoText;
     private int mSystemInfoMode;
@@ -340,11 +339,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         mNextAlarmIcon.setImageTintList(ColorStateList.valueOf(fillColor));
         mRingerModeIcon.setImageTintList(ColorStateList.valueOf(fillColor));
 
-        mBatteryMeterView = findViewById(R.id.battery);
-        mBatteryMeterView.setForceShowPercent(true);
-        mBatteryMeterView.setOnClickListener(this);
-        mBatteryMeterView.setPercentShowMode(BatteryMeterView.MODE_ESTIMATE);
-
         mClockView = findViewById(R.id.clock);
         mClockView.setOnClickListener(this);
         mClockView.setClockHideableByUser(false);
@@ -370,9 +364,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
                 QS_SHOW_BRIGHTNESS_BUTTONS,
                 QSFooterImpl.QS_SHOW_DRAG_HANDLE);
         updateSettings();
-
-        mBatteryMeterView.setVisibility(View.VISIBLE);
-        mBatteryRemainingIcon.setVisibility(View.GONE);
     }
 
     private List<String> getIgnoredIconSlots() {
@@ -510,7 +501,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
     protected void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         updateResources();
-        mBatteryMeterView.useWallpaperTextColor(shouldUseWallpaperTextColor);
     }
 
     @Override
@@ -888,9 +878,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
                 Settings.System.putIntForUser(resolver,
                         Settings.System.SCREEN_BRIGHTNESS, math, UserHandle.USER_CURRENT);
             }
-        } else if (v == mBatteryRemainingIcon || v == mBatteryMeterView) {
-            mActivityStarter.postStartActivityDismissingKeyguard(new Intent(
-                Intent.ACTION_POWER_USAGE_SUMMARY), 0);
         }
     }
 
@@ -935,11 +922,6 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         if(mSystemInfoText != null &&  mSystemInfoIcon != null) {
             updateSystemInfoText();
         }
-
-
-        // Use SystemUI context to get battery meter colors, and let it use the default tint (white)
-        mBatteryMeterView.setColorsFromContext(mHost.getContext());
-        mBatteryMeterView.onDarkChanged(new Rect(), 0, DarkIconDispatcher.DEFAULT_ICON_TINT);
     }
 
     public void setCallback(Callback qsPanelCallback) {
