ba92ef2ff532bbc59626e3813d9d020eec840ec0
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-01-17 07:31:31 -0300

    PixelPropsUtils: Set redfin build fingerprint
    Change-Id: Ib819a2f3494cffc10590638ac260cebfd8cffe79

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -68,6 +68,7 @@ public class PixelPropsUtils {
         propsToChange.put("IS_USERDEBUG", false);
         propsToChange.put("IS_USER", true);
         propsToChange.put("TYPE", "user");
+        propsToChange.put("FINGERPRINT", "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys");
     }
 
     public static void setProps(String packageName) {
