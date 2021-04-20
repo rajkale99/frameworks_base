b7e0ddf9767a48b5440e5b274e32364d18a46f94
spkal01 <kalligeross@gmail.com>
2021-04-20 20:34:10 +0530

    Make colored Statusbar Icons optional [1/2]
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6063,6 +6063,12 @@ public final class Settings {
         public static final String LOCKSCREEN_BLUR = "lockscreen_blur";
 
         /**
+         * Whether StatusBar icons should use app icon
+         * @hide
+         */
+        public static final String STATUSBAR_ICONS_STYLE = "statusbar_icons_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6257,6 +6263,7 @@ public final class Settings {
 			PRIVATE_SETTINGS.add(QS_TILE_STYLE);
 			PRIVATE_SETTINGS.add(QS_PANEL_BG_USE_NEW_TINT);
             PRIVATE_SETTINGS.add(SYSTEMUI_PLUGIN_VOLUME);
+            PRIVATE_SETTINGS.add(STATUSBAR_ICONS_STYLE);
         }
 
         /**
